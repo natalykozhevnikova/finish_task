64c885e7fd5ebced0683a131d85cb9dcbc09475a
kozhevnikovanataly@yandex.ru <kozhevnikovanataly@yandex.ru>
2022-10-30 22:35:21 +0300

    stepthree

--- Control_task/Program.cs
+++ Control_task/Program.cs
@@ -19,3 +19,10 @@ void SecondArrayWithIf(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
